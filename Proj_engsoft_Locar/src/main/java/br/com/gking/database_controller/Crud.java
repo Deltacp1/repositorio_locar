e19ddc4240464fd819f20aea423f97ac815cf993
GkingOficial <talisoncaedoso@gmail.com>
2023-12-01 12:22:19 -0300

    Adicionado busca no bd, ainda não testado.

---
+++ Proj_engsoft_Locar/src/main/java/br/com/gking/database_controller/Crud.java
@@ -0,0 +1,55 @@
+package br.com.gking.database_controller;
+
+import br.com.delta.telalogin.Placa;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Gking
+ */
+
+public class Crud {
+    private static Placa placaView;
+    private static Connection connection = null;
+    private static String databaseUser = "postgres";
+    private static String databasePassword = "18216816";
+    
+    public Crud(Placa placaView){
+        this.placaView = placaView;
+    }
+    
+    public static void databaseConnection() throws SQLException{
+       try{
+           Class.forName("org.postgresql.Driver");
+           connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/LocarDatabase", databaseUser, databasePassword);
+       } catch(ClassNotFoundException e) {
+           System.out.println("Driver não localizado: " + e.getMessage());
+       } catch(SQLException ex) {
+           System.out.println("Erro ao acessar o banco: " + ex.getMessage());
+       } finally {
+           if(connection != null){
+               connection.close();
+           }
+       }
+    }
+    
+    public static void buscarPlaca() throws SQLException{
+        String placa = placaView.getjTextField2Placa().getText();
+        
+        databaseConnection();
+        
+        String sqlQuery = "select * from carro where placa = '"+placa+"'";
+        
+        ResultSet resultPlaca = connection.createStatement().executeQuery(sqlQuery);
+        
+        if(resultPlaca.next()){
+            System.out.println("Placa: " + resultPlaca.getString("placa"));
+        } else {
+            JOptionPane.showMessageDialog(placaView, "Placa não consta no banco de dados!");
+        }
+    }
+}
